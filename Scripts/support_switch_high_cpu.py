e1651bd421a2737ebc8891846c02983b60c91a3b
Data Support ALE <data.emea@gmail.com>
2022-03-29 11:25:38 +0200

    "Update of high cpu and high memory scripts with new commands to prevent running script remotely"

--- Scripts/support_switch_high_cpu.py
+++ Scripts/support_switch_high_cpu.py
@@ -6,13 +6,10 @@ from time import  strftime, localtime, sleep
 import sys
 import os
 import datetime
-from support_tools_OmniSwitch import get_credentials, get_tech_support_sftp, get_file_sftp
+from support_tools_OmniSwitch import get_credentials, get_tech_support_sftp, ssh_connectivity_check, send_file
 from support_send_notification import send_message
 import subprocess
-import requests
-import paramiko
 import json
-import pysftp
 from database_conf import *
 
 switch_user, switch_password, mails, jid, ip_server, login_AP, pass_AP, tech_pass, random_id, company = get_credentials()
@@ -57,52 +54,95 @@ except Exception as error:
     print(error)
     pass 
 
-cmd = "python3 /flash/python/get_logs_high_cpu.py"
+text = "More logs about the switch : {0} \n\n\n".format(ipadd)
 
-#### Old method ####
-#text = "More logs about the switch : {0} \n\n\n".format(ipadd)
-#for switch_cmd in l_switch_cmd:
-#   cmd = "sshpass -p {0} ssh -o StrictHostKeyChecking=no  {1}@{2} {3}".format(switch_password,switch_user,ipadd,switch_cmd)
-#   run=cmd.split()
-#   p = subprocess.Popen(run, stdout=subprocess.PIPE,  stderr=subprocess.PIPE)
-#   out, err = p.communicate()
-#   out=out.decode('UTF-8').strip()
-
-#   text = "{0}{1}: \n{2}\n\n".format(text,switch_cmd,out) 
-try:
-      p = paramiko.SSHClient()
-      p.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-      p.connect(ipadd, port=22, username=switch_user, password=switch_password)
-      logging.info(' Connecting to OmniSwitch ' + ipadd)
-except paramiko.ssh_exception.AuthenticationException:
-        print("Authentication failed - please enter valid user name and password")
-        info = ("SSH Authentication failed when connecting to OmniSwitch {0}, we cannot collect logs").format(ipadd)
-        send_message(info,jid)
-        sys.exit(0)
-
-except paramiko.ssh_exception.NoValidConnectionsError:
-        print("Device unreachable")
-        info = ("OmniSwitch {0} is unreachable, we cannot collect logs").format(ipadd)
-        send_message(info,jid)
-        sys.exit(0)
-
-stdin, stdout, stderr = p.exec_command(cmd)
-exception = stderr.readlines()
-exception = str(exception)
-connection_status = stdout.channel.recv_exit_status()
-if connection_status != 0:
-        info = ("The python script execution on OmniSwitch {0} failed - {1}").format(ipadd,exception)
-        send_message(info,jid)
-        try:
-            write_api.write(bucket, org, [{"measurement": str(os.path.basename(__file__)), "tags": {"IP": ipadd, "Result": exception}, "fields": {"count": 1}}])
-        except UnboundLocalError as error:
-            print(error)
-            sys.exit()
-        except Exception as error:
-            print(error)
-            pass 
-        sys.exit()
+l_switch_cmd = []
+l_switch_cmd.append('echo \"date\" | su')
+l_switch_cmd.append("show virtual-chassis topology")
+l_switch_cmd.append("show chassis")
+l_switch_cmd.append("show system")
+l_switch_cmd.append("show health")
+l_switch_cmd.append("show health all cpu")
+l_switch_cmd.append("show health all memory")
+l_switch_cmd.append("show health all rx")
+l_switch_cmd.append("show health all txrx")
+l_switch_cmd.append('echo \"top -d 5 -n 5\" | su')
+l_switch_cmd.append('echo \"free -m\" | su')
 sleep(2)
+l_switch_cmd.append('echo \"free -m\" | su')
+l_switch_cmd.append('echo \"top -b -n 1 | head\" | su')
+sleep(2)
+l_switch_cmd.append('echo \"top -b -n 1 | head\" | su')
+sleep(2)
+l_switch_cmd.append('echo \"top -b -n 1 | head\" | su')
+sleep(2)
+l_switch_cmd.append('echo \"top -b -n 1 | head\" | su')
+sleep(2)
+l_switch_cmd.append('echo \"top -b -n 1 | head\" | su')
+sleep(2)
+l_switch_cmd.append('echo \"top -b -n 1 | head\" | su')
+sleep(2)
+l_switch_cmd.append('echo \"top -b -n 1 | head\" | su')
+
+for switch_cmd in l_switch_cmd:
+        try:
+            output = ssh_connectivity_check(switch_user, switch_password, ipadd, switch_cmd)
+            if output != None:
+                output = str(output)
+                output_decode = bytes(output, "utf-8").decode("unicode_escape")
+                output_decode = output_decode.replace("', '","")
+                output_decode = output_decode.replace("']","")
+                output_decode = output_decode.replace("['","")
+                text = "{0}{1}: \n{2}\n\n".format(text, switch_cmd, output_decode)
+            else:
+                exception = "Timeout"
+                info = ("Timeout when establishing SSH Session to OmniSwitch {0}, we cannot collect logs").format(ipadd)
+                print(info)
+                os.system('logger -t montag -p user.info ' + info)
+                send_message(info, jid)
+                try:
+                    write_api.write(bucket, org, [{"measurement": "support_ssh_exception", "tags": {"Reason": "CommandExecution", "IP_Address": ipadd, "Exception": exception}, "fields": {"count": 1}}])
+                except UnboundLocalError as error:
+                    print(error)
+                except Exception as error:
+                    print(error)
+                    pass 
+                sys.exit()
+        except subprocess.TimeoutExpired as exception:
+            info = ("The python script execution on OmniSwitch {0} failed - {1}").format(ipadd, exception)
+            print(info)
+            os.system('logger -t montag -p user.info ' + info)
+            send_message(info, jid)
+            try:
+                write_api.write(bucket, org, [{"measurement": "support_ssh_exception", "tags": {"Reason": "CommandExecution", "IP_Address": ipadd, "Exception": exception}, "fields": {"count": 1}}])
+            except UnboundLocalError as error:
+                print(error)
+            except Exception as error:
+                print(error)
+                pass 
+            sys.exit()
+        except subprocess.SubprocessError as exception:
+            info = ("The python script execution on OmniSwitch {0} failed - {1}").format(ipadd, exception)
+            print(info)
+            os.system('logger -t montag -p user.info ' + info)
+            send_message(info, jid)
+            try:
+                write_api.write(bucket, org, [{"measurement": "support_ssh_exception", "tags": {"Reason": "CommandExecution", "IP_Address": ipadd, "Exception": exception}, "fields": {"count": 1}}])
+            except UnboundLocalError as error:
+                print(error)
+            except Exception as error:
+                print(error)
+                pass 
+            sys.exit()
+date = datetime.date.today()
+date_hm = datetime.datetime.today()
+
+filename = "{0}_{1}-{2}_{3}_high_cpu_logs".format(date, date_hm.hour, date_hm.minute, ipadd)
+filename_path = ('/opt/ALE_Script/{0}.txt').format(filename)
+f_logs = open(filename_path, 'w')
+f_logs.write(text)
+f_logs.close()
+category = "high_cpu"
 
 try:
     write_api.write(bucket, org, [{"measurement": str(os.path.basename(__file__)), "tags": {"IP": ipadd, "Result": "Script executed on Switch"}, "fields": {"count": 1}}])
@@ -116,22 +156,16 @@ except Exception as error:
 filename='{0}_logs.txt'.format(host)
 remoteFilePath = '/flash/python/{0}'.format(filename)
 localFilePath = "/tftpboot/{0}_{1}-{2}_{3}_{4}_high_cpu".format(date,date_hm.hour,date_hm.minute,ipadd,filename) 
-### Old method for collecting logs with pysftp
-#cnopts = pysftp.CnOpts()
-#cnopts.hostkeys = None
-#with pysftp.Connection(host=ipadd, username="admin", password="switch",cnopts=cnopts) as sftp:
-#        sftp.get(remoteFilePath, localFilePath)
 
-sleep(2)
-#### Download log file by SFTP with Paramiko ###
-get_file_sftp(switch_user, switch_password, ipadd, remoteFilePath, localFilePath)
-
-info = "Process finished and logs downloaded"
-os.system('logger -t montag -p user.info Process finished and logs downloaded for High CPU issue ')
-print(localFilePath)
+subject = ("Preventive Maintenance Application - High CPU issue detected on switch: {0}").format(ipadd)
+action = ("A High CPU issue has been detected in switch(Hostname: {0}) and we have collected logs as well as Tech-Support eng complete archive").format(host)
+result = "Find enclosed to this notification the log collection for further analysis"
+category = "high_cpu"
 
 ### TECH-SUPPORT ENG COMPLETE ###
 get_tech_support_sftp(switch_user, switch_password, host, ipadd)
+
+send_file(filename_path, subject, action, result,category)
 
 try:
     write_api.write(bucket, org, [{"measurement": str(os.path.basename(__file__)), "tags": {"IP": ipadd, "Result": "Logs collected"}, "fields": {"count": 1}}])
