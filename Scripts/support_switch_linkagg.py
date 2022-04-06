c27462a01eadfb116d734b3028cde381e526b21c
Data Support ALE <data.emea@gmail.com>
2022-04-06 15:24:06 +0200

    "support_switch_linkagg if remote system not AP or no lldp info we only collect switch logs, if OAW-AP we propose to collect AP logs"

--- Scripts/support_switch_linkagg.py
+++ Scripts/support_switch_linkagg.py
@@ -48,18 +48,19 @@ with open("/var/log/devices/lastlog_linkagg.json", "r", errors='ignore') as log_
  
     ## Sample Log
     ## OS6860E_VC_Core swlogd linkAggCmm main INFO: Receive agg port leave request: agg: 1, port: 1/1/1(0)
-
+    os.system('logger -t montag -p user.info Executing script support_switch_linkagg - start')
     try:
         agg, port = re.findall(r"Receive agg port leave request: agg: (.*?), port:(.*?)\(", msg)[0]
         print(port)
     except IndexError:
         print("Index error in regex")
+        os.system('logger -t montag -p user.info Executing script support_switch_linkagg - Index error in regex')
         exit()
         
 # always 1
 #never -1
 # ? 0
-save_resp = check_save(ip, port, "linkagg")
+save_resp = check_save(ip,port, "linkagg")
 
 function = "linkagg"
 if script_has_run_recently(300,ip,function):
@@ -71,18 +72,25 @@ if script_has_run_recently(300,ip,function):
 if save_resp == "0":
     answer = "0"
     port_monitoring(switch_user, switch_password, port, ip)
-
-    lldp_port_description, lldp_mac_address = collect_command_output_lldp_port_description(switch_user, switch_password, port, ip)
-    device_ip = get_arp_entry(switch_user, switch_password, lldp_mac_address, ip)
+    notif = "A LinkAgg Port Leave occurs on OmniSwitch " + host + " port " + port + " LinkAgg " + agg
+    send_message(notif, jid)   
+    lldp_port_description, lldp_mac_address = collect_command_output_lldp_port_description(switch_user, switch_password, port, ip)  
     if lldp_port_description == 0:
-       filename_path, subject, action, result, category = collect_command_output_linkagg(switch_user, switch_password, agg, host, ip)
-       send_file(filename_path, subject, action, result, category) 
+        device_type = 0
+        filename_path, subject, action, result, category = collect_command_output_linkagg(switch_user, switch_password, agg, host, ip)
+        send_file(filename_path, subject, action, result, category) 
     elif "OAW-AP" in str(lldp_port_description):
+        # If LLDP Remote System is Stellar AP we search for IP Address in ARP Table
+        device_ip = get_arp_entry(switch_user, switch_password, lldp_mac_address, ip)
+        device_type = "OAW-AP"
         notif = "A LinkAgg Port Leave occurs on OmniSwitch " + host + " port " + port + " LinkAgg " + agg + " - Port Description: " + lldp_port_description + ". Stellar AP " + device_ip + "/" + lldp_mac_address + " is connected to this port, do you want to collect AP logs? The port-monitoring capture of port " + port + " is available on Server " + ip_server + " directory /tftpboot/"
+        # If LLDP Remote System is Stellar AP we propose to collect AP Logs
         answer = send_message_request(notif, jid)      
     else:
-        notif = "A LinkAgg Port Leave occurs on OmniSwitch " + host + " port " + port + " LinkAgg " + agg + " - Port Description: " + lldp_port_description + ". Device information: " + device_ip + "/" + lldp_mac_address + " is connected to this port, do you want to collect logs? The port-monitoring capture of port " + port + " is available on Server " + ip_server + " directory /tftpboot/"
-        answer = send_message_request(notif, jid)
+        device_type = "Others"
+        filename_path, subject, action, result, category = collect_command_output_linkagg(switch_user, switch_password, agg, host, ip)
+        action = "A LinkAgg Port Leave occurs on OmniSwitch " + host + " port " + port + " LinkAgg " + agg + " - Port Description: " + lldp_port_description + ". The port-monitoring capture of port " + port + " is available on Server " + ip_server + " directory /tftpboot/"
+        send_file(filename_path, subject, action, result, category) 
     print(answer)
     sleep(2)
     #### Download port monitoring capture ###
@@ -94,9 +102,9 @@ if save_resp == "0":
     except:
         pass
     if answer == "2":
-        add_new_save(ip, port, "linkagg", choice="always")
+        add_new_save(ip,port, "linkagg", choice="always")
     elif answer == "0":
-        add_new_save(ip, port, "linkagg", choice="never")
+        add_new_save(ip,port, "linkagg", choice="never")
 elif save_resp == "-1":
     info = "A LinkAgg Port Leave has been detected on your network from the port {0}/Agg {1} on device {2}. Decision saved for this switch/port is set to Never, we do not proceed further".format(port, agg, ip)
     send_message(info,jid)
@@ -122,11 +130,17 @@ else:
     send_message(info,jid)
 
 if answer == '1':
-    os.system('logger -t montag -p user.info Process terminated')
-    cmd = "ssudo tech_support_command 12 " + ip_server
-    ssh_connectivity_check(switch_user, switch_password, ip, cmd)
-    filename_path, subject, action, result, category = collect_command_output_linkagg(switch_user, switch_password, agg, host, ip)
-    send_file(filename_path, subject, action, result, category)
+    if device_type == "OAW-AP":
+        os.system('logger -t montag -p user.info Collecting Snapshot logs on Stellar AP')
+        cmd = "ssudo tech_support_command 12 " + ip_server
+        ssh_connectivity_check(login_AP, pass_AP, device_ip, cmd)
+        os.system('logger -t montag -p user.info Collecting logs on OmniSwitch')
+        filename_path, subject, action, result, category = collect_command_output_linkagg(switch_user, switch_password, agg, host, ip)
+        send_file(filename_path, subject, action, result, category)
+    else:
+        os.system('logger -t montag -p user.info Collecting logs on OmniSwitch')
+        filename_path, subject, action, result, category = collect_command_output_linkagg(switch_user, switch_password, agg, host, ip)
+        send_file(filename_path, subject, action, result, category)
 
 elif answer == '2':
     os.system('logger -t montag -p user.info Process terminated')
