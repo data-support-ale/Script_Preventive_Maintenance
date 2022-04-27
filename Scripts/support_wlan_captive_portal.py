7a49e2d4df4b0233cf568726b656318717a69c06
Data Support ALE <data.emea@gmail.com>
2022-04-27 16:47:17 +0200

    "Adding script for Captive Portal Online/Offline count"

--- Scripts/support_wlan_captive_portal.py
+++ Scripts/support_wlan_captive_portal.py
@@ -22,40 +22,44 @@ switch_user, switch_password, mails, jid, ip_server, login_AP, pass_AP, tech_pas
 
 last = ""
 nbr = 0
-with open("/var/log/devices/lastlog_wlan_cp.json", "r", errors='ignore') as log_file:
+with open("/var/log/devices/lastlog_captiveportal.json", "r", errors='ignore') as log_file:
     for line in log_file:
         last = line
 
-with open("/var/log/devices/lastlog_wlan_cp.json", "w", errors='ignore') as log_file:
+with open("/var/log/devices/lastlog_captiveportal.json", "w", errors='ignore') as log_file:
     log_file.write(last)
 
-with open("/var/log/devices/lastlog_wlan_cp.json", "r", errors='ignore') as log_file:
+with open("/var/log/devices/lastlog_captiveportal.json", "r", errors='ignore') as log_file:
     try:
         log_json = json.load(log_file)
         ip = log_json["relayip"]
         host = log_json["hostname"]
         msg = log_json["message"]
     except json.decoder.JSONDecodeError:
-        print("File /var/log/devices/lastlog_wlan_cp.json empty")
+        print("File /var/log/devices/lastlog_captiveportal.json empty")
         exit()
     except IndexError:
         print("Index error in regex")
         exit()
+    f = msg.split(',')
+    for element in f:
+        if "portal" in element:
+            try:
+               status = re.findall(r"portal/(.*)", msg)[0]
+               print(status)
+            except IndexError:
+                print("Index error in regex")
+                exit()
+        elif "open" in element:
+            try:
+               status = re.findall(r"open/(.*)", msg)[0]
+               print(status)
+            except IndexError:
+                print("Index error in regex")
+                exit()
 
-    try:
-        status = re.findall(r"portal/(.*)", msg)[0]
-        print(status)
-    except IndexError:
-        print("Index error in regex")
-        exit()
-    if status == "ONLINE":
-        nbr = nbr + 1
-    if status == "OFFLINE":
-        nbr = nbr - 1
-    print(nbr)
-   
 try:
-    write_api.write(bucket, org, [{"measurement": str(os.path.basename(__file__)), "tags": {"Nbr of Users": nbr}, "fields": {"count": 1}}])
+    write_api.write(bucket, org, [{"measurement": str(os.path.basename(__file__)), "tags": {"status": status}, "fields": {"count": 1}}])
 except UnboundLocalError as error:
     print(error)
     sys.exit()
