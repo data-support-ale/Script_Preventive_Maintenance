1fffe0264b514808db13da35e9d68771b942c466
Data Support ALE <data.emea@gmail.com>
2022-06-28 09:47:14 +0200

    "New script port flapping with logs and script for testing"

--- Scripts/support_switch_port_flapping.py
+++ Scripts/support_switch_port_flapping.py
@@ -25,14 +25,12 @@ def process(ip, hostname, port, link):
         print(error)
         pass 
 
+        lldp_port_description, _ = collect_command_output_lldp_port_description(switch_user, switch_password, port, ip)
+        status_changes, link_quality = collect_command_output_flapping(switch_user, switch_password, port, ip)
+
     save_resp = check_save(ip, port, "flapping")
 
     if link == "UPLINK":
-        
-        lldp_port_description, _ = collect_command_output_lldp_port_description(
-        switch_user, switch_password, port, ip)
-
-        status_changes, link_quality = collect_command_output_flapping(switch_user, switch_password, port, ip)
         info = "A port flapping has been detected on your network on the UPLINK port {0} on OmniSwitch {1}/{2}\nInterface details :\
         \n- System Description : {3}\n- Number of Status Change : {4}\n- Link-Quality : {5}\n\nPlease check the number of status \
         change and Link-Quality level (command show interfaces port x/x/x status).\nWe could consider this issue is related to \
@@ -42,14 +40,12 @@ def process(ip, hostname, port, link):
     else:
 
         if save_resp == "0":
-            lldp_port_description, _ = collect_command_output_lldp_port_description(switch_user, switch_password, port, ip)
-            status_changes, link_quality = collect_command_output_flapping(switch_user, switch_password, port, ip)
             if lldp_port_description == 0:
                 info = "A port flapping has been detected on your network on the access port {0} - System Description: N/A - Number of Status Change : {2} - Link Quality : {3} on OmniSwitch {4}/{5}.\nIf you click on Yes, the following actions will be done: Port Admin Down/Up.".format(
                     port, lldp_port_description, status_changes, link_quality, ip, hostname)
                 answer = send_message_request_advanced(info, jid, "Admin down")
             else:
-                info = "A port flapping has been detected on your network on the access port {0} - System Description: {1} - Number of Status Change : {2} - Link Quality : {3} on OmniSwitch {4}/{5}.\nIf you click on Yes, the following actions will be done: Port Admin Down/Up.".format(
+                info = "Preventive Maintenance Application - A port flapping has been detected on your network on the access port {0} - System Description: {1} - Number of Status Change : {2} - Link Quality : {3} on OmniSwitch {4}/{5}.\nIf you click on Yes, the following actions will be done: Port Admin Down/Up.".format(
                     port, lldp_port_description, status_changes, link_quality, ip, hostname)
                 answer = send_message_request_advanced(info, jid, "Admin down")
             if answer == "2":
@@ -71,7 +67,7 @@ def process(ip, hostname, port, link):
             os.system('logger -t montag -p user.info Process terminated')
 
 
-            info = "A port flapping has been detected on your network and the port {0} is administratively updated  on OmniSwitch {1}/{2}".format(port, ip, hostname)
+            info = "Preventive Maintenance Application - A port flapping has been detected on your network and the port {0} is administratively updated  on OmniSwitch {1}/{2}".format(port, ip, hostname)
             send_message(info, jid)
 
             # disable_debugging
@@ -92,7 +88,7 @@ def process(ip, hostname, port, link):
             ssh_connectivity_check(switch_user, switch_password, ip, cmd)
             os.system('logger -t montag -p user.info Port {0} of OmniSwitch {1}/{2} disable'.format(port, ip, hostname))
 
-            info = "A port flapping has been detected on your network and the port {0} is administratively down  on OmniSwitch {1}/{2}".format(port, ip, hostname)
+            info = "Preventive Maintenance Application - A port flapping has been detected on your network and the port {0} is administratively down  on OmniSwitch {1}/{2}".format(port, ip, hostname)
             send_message(info, jid)
 
             # disable_debugging
@@ -127,7 +123,7 @@ with open("/var/log/devices/lastlog_flapping.json", "r", errors='ignore') as log
     for line in reversed(lines):
         try:
             line_json = json.loads(line)
-            timestamp = (str(line_json["@timestamp"])[:-6]).replace("T", " ").replace(":", " ").replace("-", " ")
+            timestamp = (str(line_json["@timestamp"])[:19]).replace("T", " ").replace(":", " ").replace("-", " ")
             time = datetime.strptime(timestamp, "%Y %m %d %H %M %S")
             if counter == 0:
                 counter = 1
@@ -144,8 +140,7 @@ with open("/var/log/devices/lastlog_flapping.json", "r", errors='ignore') as log
             print("File /var/log/devices/lastlog_flapping.json empty")
             exit()
         except IndexError:
-            print("Index error in regex")
-            exit()
+            pass
 
 with open("/var/log/devices/lastlog_flapping.json", "w", errors='ignore') as log_file:
     log_file.write(text_file)
@@ -160,29 +155,26 @@ with open("/var/log/devices/lastlog_flapping.json", "r", errors='ignore') as log
             hostname = log_json["hostname"]
             msg = log_json["message"]
             port, state = re.findall(r"LINKSTS (.*?) (UP|DOWN)", msg)[0]
-
-            if(ip == last_ip and port == last_port):
-                counter+= 1
-                print(counter)
-
-            if(counter == 5):
-
-                lldp_port_capability = collect_command_output_lldp_port_capability(
-                switch_user, switch_password, port, ip)
-
-                if("Router" in str(lldp_port_capability) or "Bridge" in str(lldp_port_capability)):
-                    process(ip, hostname, port, "UPLINK")
-                elif(isUpLink(switch_user, switch_password, port, ip)):
-                    process(ip, hostname, port, "UPLINK")
-
-                else:
-                    process(ip, hostname, port, "ACCESS")
-
-                break
-
+            print("log: {} {}".format(ip, port))
         except json.decoder.JSONDecodeError:
             print("File /var/log/devices/lastlog_flapping.json empty")
             exit()
         except IndexError:
-            print("Index error in regex")
-            exit()
+            pass
+
+        if(ip == last_ip and port == last_port):
+            counter+= 1
+            print(counter)
+
+        if(counter == 5):
+            #set_portnumber(port)
+            lldp_port_capability = collect_command_output_lldp_port_capability(
+            switch_user, switch_password, port, ip)
+
+            if("Router" in str(lldp_port_capability) or "Bridge" in str(lldp_port_capability)):
+                process(ip, hostname, port, "UPLINK")
+            elif(isUpLink(switch_user, switch_password, port, ip)):
+                process(ip, hostname, port, "UPLINK")
+            else:
+                process(ip, hostname, port, "ACCESS")
+            break
