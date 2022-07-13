d2b6ca9b08006d611457d5cc6dab9b58f5b91ab3
Data Support ALE <data.emea@gmail.com>
2022-07-13 09:44:00 +0200

    "Adding Timeout 600 seconds into the Rainbow Adaptive Card in the workflow json file and adding new script and debian package for testing"

---
+++ Testing/support_wlan_test2.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import re
+import json
+from support_tools_OmniSwitch import get_credentials
+from time import strftime, localtime, sleep
+from support_send_notification import *
+# from database_conf import *
+import time
+
+from pattern import set_rule_pattern, set_portnumber, set_decision, mysql_save
+
+path = os.path.dirname(__file__)
+print(os.path.abspath(os.path.join(path,os.pardir)))
+sys.path.insert(1,os.path.abspath(os.path.join(path,os.pardir)))
+from alelog import alelog
+
+# Script init
+script_name = sys.argv[0]
+os.system('logger -t montag -p user.info Executing script ' + script_name)
+
+pattern = sys.argv[1]
+print(pattern)
+set_rule_pattern(pattern)
+# info = ("We received following pattern from RSyslog {0}").format(pattern)
+# os.system('logger -t montag -p user.info ' + info)
+
+runtime = strftime("%d_%b_%Y_%H_%M_%S", localtime())
+_runtime = strftime("%Y-%m-%d %H:%M:%S", localtime())
+
+# Get informations from logs.
+switch_user, switch_password, mails, jid1, jid2, jid3, ip_server, login_AP, pass_AP, tech_pass, random_id, company = get_credentials()
+
+
+print("test2")
