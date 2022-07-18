b498f6bd5f05937848a68c788a3cc63759bd4b33
Data Support ALE <data.emea@gmail.com>
2022-07-18 17:35:02 +0200

    "Adding logging"

--- Scripts/support_wlan_get_log.py
+++ Scripts/support_wlan_get_log.py
@@ -7,8 +7,9 @@ import sys
 from time import gmtime, strftime, localtime
 from support_tools_Stellar import get_credentials, collect_logs
 from support_send_notification import send_message, send_file
+import syslog
 
-
+syslog.openlog('support_wlan_get_log')
 runtime = strftime("%d_%b_%Y_%H_%M_%S_0000", localtime())
 uname = os.system('uname -a')
 system_name = os.uname()[1].replace(" ", "_")
@@ -32,21 +33,27 @@ with open("/var/log/devices/get_log_ap.json", "r", errors='ignore') as log_file:
         ipadd = log_json["relayip"]
         host = log_json["hostname"]
         msg = log_json["message"]
+        print(msg)
+        syslog.syslog(syslog.LOG_DEBUG, "Syslog IP Address: " + ipadd)
+        syslog.syslog(syslog.LOG_DEBUG, "Syslog Hostname: " + host)
+        #syslog.syslog(syslog.LOG_DEBUG, "Syslog message: " + msg)
     except json.decoder.JSONDecodeError:
-        print("/var/log/devices/get_log_ap.json empty")
+        print("File /var/log/devices/get_log_ap.json empty")
+        syslog.syslog(syslog.LOG_INFO, "File /var/log/devices/get_log_ap.json - JSONDecodeError")
+        exit()
+    except IndexError:
+        print("Index error in regex")
+        syslog.syslog(syslog.LOG_INFO, "File /var/log/devices/get_log_ap.json - Index error in regex")
         exit()
 
-# get the paswd with the technical support code
-#cmd = "sshpass -p {0} ssh -v -o StrictHostKeyChecking=no {1}@{2} genrpd {3}".format(pass_AP, login_AP, ip, tech_pass)
-#run = cmd.split()
-#p = subprocess.Popen(run, stdout=subprocess.PIPE,  stderr=subprocess.PIPE)
-#out, err = p.communicate()
-#pass_root = out.decode('ascii').strip()
-# Collect snapshot and additionnal logs
-#cmd = "/usr/sbin/take_snapshot.sh start {}".format(ip_server)
-
+syslog.syslog(syslog.LOG_INFO, "Executing function collect_logs")
 filename_path, subject, action, result, category = collect_logs(login_AP, pass_AP, ipadd, pattern)
+syslog.syslog(syslog.LOG_INFO, "Subject: " + subject)
+syslog.syslog(syslog.LOG_INFO, "Action: " + action)
+syslog.syslog(syslog.LOG_INFO, "Result: " + result)
+syslog.syslog(syslog.LOG_INFO, "Logs collected - Calling VNA API - Send File")            
 send_file(filename_path, subject, action, result, category, jid)
+syslog.syslog(syslog.LOG_INFO, "Logs collected - Notification sent")
 
 # clear log file
 open('/var/log/devices/get_log_ap.json', 'w').close()
