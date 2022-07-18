8b43efd4b5432e9198d01694db7a2265fffce5ba
Data Support ALE <data.emea@gmail.com>
2022-07-18 11:12:02 +0200

    "Adding 4th option for disabling port or linkagg"

--- Scripts/support_switch_violation.py
+++ Scripts/support_switch_violation.py
@@ -142,10 +142,11 @@ if save_resp == "0":
     syslog.syslog(syslog.LOG_INFO, "No decision saved")
     if agg_id == "0":
         syslog.syslog(syslog.LOG_INFO, "Port is not member of a LinkAgg")
-        notif = ("Preventive Maintenance Application - A port violation occurs on OmniSwitch {0} / {1}. Port : {2} - Source : {3}.\nDo you want to clear the violation?").format(host,ipadd,port,reason)
+        feature = "Disable Port " + port
+        notif = ("Preventive Maintenance Application - A port violation occurs on OmniSwitch {0} / {1}. Port : {2} - Source : {3}.\nDo you want to clear the violation or administratively disable the Interface/Port?").format(host,ipadd,port,reason)
         syslog.syslog(syslog.LOG_INFO, "Notification: " + notif)
-        syslog.syslog(syslog.LOG_INFO, "Logs collected - Calling VNA API - Rainbow Adaptive Card")
-        answer = send_message_request(notif, jid)
+        syslog.syslog(syslog.LOG_INFO, "Logs collected - Calling VNA API - Rainbow Adaptive Card of type Advanced")
+        answer = send_message_request_advanced(notif, jid,feature)
         syslog.syslog(syslog.LOG_INFO, "Logs collected - Notification sent")
         if answer == "2":
             add_new_save(ipadd, port, "violation", choice="always")
@@ -157,10 +158,11 @@ if save_resp == "0":
 
     else:
         syslog.syslog(syslog.LOG_INFO, "Port is member of a LinkAgg")
-        notif = "Preventive Maintenance Application - A port violation occurs on OmniSwitch " + host + " LinkAgg ID " + agg_id + ", source: " + reason + ".\nDo you want to clear the violation? " + ip_server
+        feature = "Disable port " + agg_id
+        notif = "Preventive Maintenance Application - A port violation occurs on OmniSwitch " + host + " LinkAgg ID " + agg_id + ", source: " + reason + ".\nDo you want to clear the violation or administratively disable the Link Aggregate? " + ip_server
         syslog.syslog(syslog.LOG_INFO, "Notification: " + notif)
-        syslog.syslog(syslog.LOG_INFO, "Logs collected - Calling VNA API - Rainbow Adaptive Card")
-        answer = send_message_request(notif, jid)
+        syslog.syslog(syslog.LOG_INFO, "Logs collected - Calling VNA API - Rainbow Adaptive Card of type Advanced")
+        answer = send_message_request_advanced(notif, jid,feature)
         syslog.syslog(syslog.LOG_INFO, "Logs collected - Notification sent")
         if answer == "2":
             add_new_save(ipadd, port, "violation", choice="always")
@@ -211,7 +213,7 @@ if answer == '1':
     filename_path = "/var/log/devices/" + host + "/syslog.log"
     syslog.syslog(syslog.LOG_INFO, "Logs attached: " + filename_path)
     category = "port_violation"
-    subject = "Preventive Maintenance Application - A port violation is detected:".format(host, ipadd)
+    subject = "Preventive Maintenance Application - A port violation has been detected:".format(host, ipadd)
     action = "Violation on OmniSwitch {0}, port {1} has been cleared up".format(host, port)
     result = "Find enclosed to this notification the log collection"
     syslog.syslog(syslog.LOG_INFO, "Subject: " + subject)
@@ -230,6 +232,35 @@ elif answer == '2':
     syslog.syslog(syslog.LOG_INFO, "Port violation cleared up")
     ## 2 seconds delay for getting port violation logs
     sleep(2)
+
+## Value 3 when we return advanced value like Disable port x/x/x
+elif answer == '3':
+    syslog.syslog(syslog.LOG_INFO, "Anwser received is " + feature)
+    # DISABLE Port
+    if agg_id == "0":
+        syslog.syslog(syslog.LOG_INFO, "SSH Session for disabling port")
+        cmd = "interfaces port " + port + " admin-state disable"
+        ssh_connectivity_check(switch_user, switch_password, ipadd, cmd)
+        syslog.syslog(syslog.LOG_INFO, "SSH Session closed")
+    else: 
+        syslog.syslog(syslog.LOG_INFO, "SSH Session for disabling linkagg")
+        cmd = "linkagg lacp agg " + agg_id + " admin-state disable"
+        ssh_connectivity_check(switch_user, switch_password, ipadd, cmd)
+        syslog.syslog(syslog.LOG_INFO, "SSH Session closed")   
+    filename_path = "/var/log/devices/" + host + "/syslog.log"
+    category = "port_violation"
+    subject = "Preventive Maintenance Application - A port violation has been detected:".format(host, ipadd)
+    if agg_id == "0":
+        action = "Port violation has been fixed by administratively disabling LinkAgg {0} on OmniSwitch {1} / {2}".format(agg_id, host, ipadd)
+    else:
+        action = "Port violation has been fixed by administratively disabling Interface/Port {0} on OmniSwitch {1} / {2}".format(port, host, ipadd)
+    result = "Find enclosed to this notification the log collection"
+    syslog.syslog(syslog.LOG_INFO, "Subject: " + subject)
+    syslog.syslog(syslog.LOG_INFO, "Action: " + action)
+    syslog.syslog(syslog.LOG_INFO, "Result: " + result)
+    syslog.syslog(syslog.LOG_INFO, "Logs collected - Calling VNA API - Send File")      
+    send_file(filename_path, subject, action, result, category, jid)
+    syslog.syslog(syslog.LOG_INFO, "Logs collected - Notification sent")
 
 else:
     print("No answer match")
